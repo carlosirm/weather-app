24b1e41200cea0d56d6d23b54b4f01c018883563
Carlos Rosas <Carlos.Rosas@endava.com>
2022-12-27 09:29:17 -0300

    uso de api en español

--- weather.py
+++ weather.py
@@ -1,11 +1,35 @@
 # weather.py
 
 import argparse
-
+import json
+import sys
 from configparser import ConfigParser
-from urllib import parse
+from urllib import error, parse, request
+import style
+
 
 BASE_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
+LANG = '&lang=es'
+
+THUNDERSTORM = range(200, 300)
+DRIZZLE = range(300, 400)
+RAIN = range(500, 600)
+SNOW = range(600, 700)
+ATMOSPHERE = range(700, 800)
+CLEAR = range(800, 801)
+CLOUDY = range(801, 900)
+
+def _get_api_key():
+    """Fetch the API key from your configuration file.
+
+    Expects a configuration file named "secrets.ini" with structure:
+
+        [openweather]
+        api_key=<YOUR-OPENWEATHER-API-KEY>
+    """
+    config = ConfigParser()
+    config.read("secrets.ini")
+    return config["openweather"]["api_key"]
 
 def read_user_cli_args():
     """ Handles the CLI user interactions.
@@ -35,27 +59,97 @@ def build_weather_query(city_input, imperial=False):
     city_name = " ".join(city_input)
     url_encoded_city_name = parse.quote_plus(city_name)
     units = "imperial" if imperial else "metric"
-    url = f"{BASE_WEATHER_API_URL}?q={url_encoded_city_name}&units={units}&appid={api_key}"
+    url = f"{BASE_WEATHER_API_URL}?q={url_encoded_city_name}&units={units}&appid={api_key}{LANG}"
 
     return url
 
-def _get_api_key():
-    """Fetch the API key from your configuration file.
 
-    Expects a configuration file named "secrets.ini" with structure:
+def get_weather_data(query_url):
+    """Makes an API request to a URL and returns the data as a Python object.
 
-        [openweather]
-        api_key=<YOUR-OPENWEATHER-API-KEY>
+    Args:
+        query_url (str): URL formatted for OpenWeather's city name endpoint
+
+    Returns:
+        dict: Weather information for a specific city
     """
-    config = ConfigParser()
-    config.read("secrets.ini")
-    return config["openweather"]["api_key"]
+    try:
+        response = request.urlopen(query_url)
+    except error.HTTPError as http_error:
+        if http_error.code == 401: #401 unauthorized
+            sys.exit("Access denied. Check your api key")
+        elif http_error.code == 404: #404 Not found
+            sys.exit("Can't find weather data for this city")
+        else:
+            sys.exit(f"Something went wrong...({http_error})")
+    
+    data = response.read()
+    
+    try:
+        return json.loads(data)
+    except json.JSONDecodeError:
+        sys.exit("couldn't read server response")
+
+def _select_weather_display_params(weather_id):
+    if weather_id in THUNDERSTORM:
+        display_params = ("💥", style.RED)
+    elif weather_id in DRIZZLE:
+        display_params = ("💧", style.CYAN)
+    elif weather_id in RAIN:
+        display_params = ("💦", style.BLUE)
+    elif weather_id in SNOW:
+        display_params = ("⛄️", style.WHITE)
+    elif weather_id in ATMOSPHERE:
+        display_params = ("🌀", style.BLUE)
+    elif weather_id in CLEAR:
+        display_params = ("🔆", style.YELLOW)
+    elif weather_id in CLOUDY:
+        display_params = ("💨", style.WHITE)
+    else:  # In case the API adds new weather codes
+        display_params = ("🌈", style.RESET)
+    return display_params
+
+
+def display_weather_info (weather_data, imperial=False):
+    """Prints formatted weather information about a city.
+
+    Args:
+        weather_data (dict): API response from OpenWeather by city name
+        imperial (bool): Whether or not to use imperial units for temperature
+
+    More information at https://openweathermap.org/current#name
+    """
+    city = weather_data["name"]
+    country = weather_data["sys"]["country"]
+    location = f"{city}, {country}"
+    
+    weather_id = weather_data["weather"][0]["id"]
+    weather_description = weather_data["weather"][0]["description"]
+    temperature = weather_data["main"]["temp"]
+
+    style.change_color(style.REVERSE)
+    print(f"{location:^{style.PADDING}}", end="")
+    style.change_color(style.RESET)
+
+    weather_symbol, color = _select_weather_display_params(weather_id)
+
+    style.change_color(color)
+    print(f"\t{weather_symbol}", end=" ")
+    print(
+        f"{weather_description.capitalize():^{style.PADDING}}",
+        end=" ",
+    )
+    style.change_color(style.RESET)
+
+    print(f"({temperature}°{'F' if imperial else 'C'})")
 
 
 if __name__ == "__main__":
     user_args = read_user_cli_args()
     query_url = build_weather_query(user_args.city, user_args.imperial)
-    print(query_url)
+    weather_data = get_weather_data(query_url)
+    display_weather_info(weather_data, user_args.imperial)
+
 
 
 
