ce3570df18b160aa122b23dba02074a6fc381d61
Carlos Rosas <Carlos.Rosas@endava.com>
2023-01-02 20:47:07 -0300

    add requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from src.line_argument import read_user_cli_args
 from src.api_manager import build_weather_query, get_url_icon
 from src.style import _select_weather_display_params, display_weather_info
-from src.weather import get_weather_json, get_weather_data, open_wind_class_data, wind_classifier
+from src.weather import get_weather_json, get_weather_data
+
 
 if __name__ == "__main__":
     user_args = read_user_cli_args()
